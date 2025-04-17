e2b9cef32b79c9ee96d18f975b3cc0a30b672cdc
360Parminder <yesask8@gmail.com>
2025-04-17 15:24:04 +0530

    feat: add PATCH method to CORS configuration for enhanced API support

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const allowedOrigins = [
         callback(new Error("Not allowed by CORS"));
       }
     },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
   };
