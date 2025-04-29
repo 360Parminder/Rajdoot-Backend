e7eb1ba2fa47dcba5d852edc4855b73c4a170e65
360Parminder <yesask8@gmail.com>
2025-04-29 22:23:27 +0530

    fix: normalize email input by converting to lowercase during user signup for consistency

--- controllers/authController.js
+++ controllers/authController.js
@@ -160,7 +160,7 @@ exports.signup = async (req, res, next) => {
     }
     const user = await User.create({
       name: req.body.name,
-      email: req.body.email,
+      email: req.body.email.toLowerCase(),
       password: req.body.password,
       passwordConfirm: req.body.passwordConfirm,
       role: req.body.role,
